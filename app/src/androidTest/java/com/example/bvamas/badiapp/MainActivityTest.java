f66c53b8d60cbf4f7e858eef6c7a289a36157329
Sivakeerthan <keerthan_1@hotmail.com>
2018-07-06 11:32:54 +0200

    latest

--- app/src/androidTest/java/com/example/bvamas/badiapp/MainActivityTest.java
+++ app/src/androidTest/java/com/example/bvamas/badiapp/MainActivityTest.java
@@ -1,10 +1,17 @@
 package com.example.bvamas.badiapp;
 
 import android.Manifest;
+import android.support.test.espresso.Espresso;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+
+import android.support.test.espresso.assertion.ViewAssertions;
+import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Rule;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(AndroidJUnit4.class)
@@ -12,4 +19,9 @@ public class MainActivityTest {
     @Rule
     public ActivityTestRule<MainActivity> mMainActivityActivityTestRule =
             new ActivityTestRule<MainActivity>(MainActivity.class);
+    @Test
+    public void clickListViewButton_opensListActivity() throws Exception{
+      onView(ViewMatchers.withId(R.id.btnlist)).perform(click());
+      onView(ViewMatchers.withId(R.id.badiliste)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
+    }
 }
